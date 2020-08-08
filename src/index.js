c706fba108803afcf6af03871dc43e611a57c6fa
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2020-08-07 19:19:04 -0700

    Componente com propriedade

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import './index.css';
 import Primeiro from './components/basicos/Primeiro';
+import ComParametro from './components/basicos/ComParametro';
 
 ReactDOM.render(
-    <div>
+    <div id="app">
       <Primeiro></Primeiro>
+      <ComParametro
+        titulo="Segundo Componente"
+        subtitulo="Muito legal!"/>
     </div>, 
   document.getElementById('root')
   );
